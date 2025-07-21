4bdc479c8a18a032e9e9fd425e7b138125e298ea
SahaHungkul <mhasbysyabani27@gmail.com>
2025-07-21 11:44:46 +0700

    menambahkan fitur user

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\RegisterController;
-use App\Http\Controllers\Api\LoginController;
-use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\Auth\RegisterController;
+use App\Http\Controllers\Api\Auth\LoginController;
+use App\Http\Controllers\Api\Auth\LogoutController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
