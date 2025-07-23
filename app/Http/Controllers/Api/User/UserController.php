7de9beba69cfea0ab169831bd3a0e7d0b0a993df
SahaHungkul <mhasbysyabani27@gmail.com>
2025-07-23 14:22:54 +0700

    ralat penambahan bukan role dan akse, melainkan penambahan user

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -3,9 +3,75 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\User\StoreUserRequest;
+use App\Http\Requests\User\UpdateUserRequest;
+use App\Http\Resources\User\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    public function index()
+    {
+        $users = User::latest()->paginate(10);
+        return UserResource::collection($users);
+    }
 
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = Hash::make($data['password']);
+
+        $user = User::create($data);
+        return new UserResource($user);
+    }
+
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        if (! $user) {
+            return response()->json([
+                'message' => 'User tidak ditemukan'
+            ], 404);
+        }
+        return new UserResource($user);
+    }
+
+    public function update(UpdateUserRequest $request, User $id)
+    {
+        $user = User::find($id);
+
+        if (! $user) {
+            return response()->json([
+                'message' => 'User tidak ditemukan.'
+            ], 404);
+        }
+
+        $data = $request->validated();
+        
+        if (!empty($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        } else {
+            unset($data['password']);
+        }
+
+        $user->update($data);
+        return new UserResource($user);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User tidak ditemukan.'
+            ], 404);
+        }
+        $user->delete();
+        return response()->json([
+            'message' => 'User berhasil dihapus.'
+        ], 200);
+    }
 }
