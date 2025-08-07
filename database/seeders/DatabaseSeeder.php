3fe025932b3bbaf4693e795231dbf3e500efa9e5
SahaHungkul <mhasbysyabani27@gmail.com>
2025-08-07 15:02:07 +0700

    feat(booking): add Booking

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\RoleAndPermissionSeeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,15 @@ class DatabaseSeeder extends Seeder
         RoleAndPermissionSeeder::class,
         SuperAdminSeeder::class,
     ]);
+
+        $user = User::firstOrCreate([
+            'email' => 'admin1123@example.com'],
+            [
+                'name' => 'Admin1',
+                'password' => hash::make('password'),
+            ]
+        );
+
+        $user->assignRole('admin');
     }
 }
